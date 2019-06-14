588cc7dd0dafd42ff42deb797200f7638fedc2c8
Lewis Franklin <github@brolewis.com>
2019-06-14 13:17:30 -0500

    QET-139: Get tests online
    Add run_tests.py, run tests through `black` and `flake8` and add the
    missed `requests-mock` library (oops 2).

--- tests/test_uuid_replacer.py
+++ tests/test_uuid_replacer.py
@@ -1,16 +1,21 @@
+"""Unit tests for the qecommon_tools.uuid_replacer."""
 import os
 
 from qecommon_tools import get_file_contents
 from qecommon_tools.uuid_replacer import uuid_replace
 
 HERE = os.path.dirname(os.path.abspath(__file__))
-INPUT_FILE = os.path.join(HERE, 'uuid-only-lines.input')
-EXPECTED_OUTPUT_FILE = os.path.join(HERE, 'uuid-only-lines.output')
+INPUT_FILE = os.path.join(HERE, "uuid-only-lines.input")
+EXPECTED_OUTPUT_FILE = os.path.join(HERE, "uuid-only-lines.output")
 
 
 def test_uuid_replacer(tmpdir):
-    testoutput_filename = tmpdir / 'test.output'
-    with testoutput_filename.open('w') as testoutput, open(INPUT_FILE, 'r') as testinput:
+    testoutput_filename = tmpdir / "test.output"
+    with testoutput_filename.open("w") as testoutput, open(
+        INPUT_FILE, "r"
+    ) as testinput:
         uuid_replace(testinput, testoutput)
 
-    assert get_file_contents(EXPECTED_OUTPUT_FILE) == get_file_contents(str(testoutput_filename))
+    assert get_file_contents(EXPECTED_OUTPUT_FILE) == get_file_contents(
+        str(testoutput_filename)
+    )

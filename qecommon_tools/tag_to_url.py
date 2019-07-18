b9101a6e6136b3f7aca9f91361975bc8e93f8a49
Lewis Franklin <github@brolewis.com>
2019-07-18 12:36:33 -0500

    Fix: Correct Key Name

--- qecommon_tools/tag_to_url.py
+++ qecommon_tools/tag_to_url.py
@@ -3,11 +3,11 @@ import re
 
 JIRA = {
     "pattern": re.compile("^([A-Z][A-Z]+-[0-9]+)$"),
-    "url_pattern": "https://jira.rax.io/browse/{}",
+    "url_template": "https://jira.rax.io/browse/{}",
 }
 SNOW = {
     "pattern": re.compile("(^[A-Z][A-Z]+[0-9]+)$"),
-    "url_pattern": "https://rackspace.service-now.com/rm_story.do"
+    "url_template": "https://rackspace.service-now.com/rm_story.do"
     "?sysparm_query=number={}&sysparm_view=scrum",
 }
 VersionOne = {"pattern": re.compile("(^[A-Z]-[0-9]+)$")}

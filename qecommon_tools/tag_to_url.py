9fb015cd75ee33ddb65502d6ed29d98ead924f0e
Lewis Franklin <github@brolewis.com>
2019-07-18 10:10:59 -0500

    Enhancement: Tag-to-URL Plugin
    Move from hard-coded ticketing systems to a plugin architecture.

---
+++ qecommon_tools/tag_to_url.py
@@ -0,0 +1,13 @@
+"""Pre-defined tag-to-url patterns."""
+import re
+
+JIRA = {
+    "pattern": re.compile("^([A-Z][A-Z]+-[0-9]+)$"),
+    "url_pattern": "https://jira.rax.io/browse/{}",
+}
+SNOW = {
+    "pattern": re.compile("(^[A-Z][A-Z]+[0-9]+)$"),
+    "url_pattern": "https://rackspace.service-now.com/rm_story.do"
+    "?sysparm_query=number={}&sysparm_view=scrum",
+}
+VersionOne = {"pattern": re.compile("(^[A-Z]-[0-9]+)$")}
